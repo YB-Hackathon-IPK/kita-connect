446c4c26f326965c6217659ad2000ff9b5e90fea
Joel Messerli <joel.messerli@inpeek.ch>
2019-11-16 10:01:39 +0100

    Implement favorite updating on server

--- src/app/parents/components/timeline-entry/timeline-entry.component.ts
+++ src/app/parents/components/timeline-entry/timeline-entry.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+  ChangeDetectorRef,
+} from '@angular/core';
 import { TimelineEntry } from '../../models/timeline-entry.model';
 import { TimelineService } from '../../services/timeline.service';
 
@@ -6,27 +12,40 @@ import { TimelineService } from '../../services/timeline.service';
   selector: 'app-timeline-entry',
   templateUrl: './timeline-entry.component.html',
   styleUrls: ['./timeline-entry.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TimelineEntryComponent {
   @Input() entry: TimelineEntry;
 
-  constructor(private timelineService: TimelineService){
-  }
+  constructor(
+    private timelineService: TimelineService,
+    private changeDetector: ChangeDetectorRef
+  ) {}
 
   public isValidSentiment(): boolean {
-    return this.entry.sentiment === 0 || this.entry.sentiment === 1 || this.entry.sentiment === 2;
+    return (
+      this.entry.sentiment === 0 ||
+      this.entry.sentiment === 1 ||
+      this.entry.sentiment === 2
+    );
   }
 
   public updateFavorite() {
-    this.timelineService.updateFavorite({
-      id: this.entry.id,
-      favorite: !this.entry.favorite
-    })
-        .subscribe();
+    this.timelineService
+      .updateFavorite({
+        id: this.entry.id,
+        child: this.entry.child,
+        favorite: !this.entry.favorite,
+      })
+      .subscribe(response => {
+        if (response.status === 'ok') {
+          this.entry.favorite = !this.entry.favorite;
+          this.changeDetector.markForCheck();
+        }
+      });
   }
 
   get concatenatedTags(): string {
-    return this.entry.tags.reduce((tags, tag) => `${tags} #${tag}`,'')
+    return this.entry.tags.reduce((tags, tag) => `${tags} #${tag}`, '');
   }
 }
--- src/app/parents/services/timeline.service.ts
+++ src/app/parents/services/timeline.service.ts
@@ -3,37 +3,54 @@ import { Timeline } from '../models/timeline.model';
 import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { GroupedTimeline } from '../models/grouped-timeline.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { TimelineEntry } from '../models/timeline-entry.model';
 
 @Injectable()
 export class TimelineService {
   private readonly millisecondsInADay = 86_400_000;
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient) {}
+
+  public fetchTimeline(
+    filter: (entry: TimelineEntry) => boolean = () => true
+  ): Observable<GroupedTimeline> {
+    return this.httpClient
+      .get<{ data: Timeline }>(
+        'https://ybhack-ipk-fn.azurewebsites.net/api/timeline?child=Adrienne+Ponce'
+      )
+      .pipe(
+        map(data => data.data),
+        map(entries => this.groupAndSortByDay(entries, filter)),
+        catchError(() => of([]))
+      );
   }
 
-  public fetchTimeline(filter: (entry: TimelineEntry) => boolean = () => true): Observable<GroupedTimeline> {
-    return this.httpClient.get<{ data: Timeline }>(
-        'https://ybhack-ipk-fn.azurewebsites.net/api/timeline?child=Adrienne+Ponce')
-        .pipe(
-            map(data => data.data),
-            map(entries => this.groupAndSortByDay(entries, filter)),
-            catchError(() => of([]))
-        );
+  public updateFavorite(data: {
+    id: string;
+    child: string;
+    favorite: boolean;
+  }): Observable<any> {
+    // let params = new HttpParams();
+    // params.set('child', data.child);
+    // params.set('id', data.id);
+
+    return this.httpClient.put<any>(
+      `https://ybhack-ipk-fn.azurewebsites.net/api/favorite?id=${
+        data.id
+      }&child=${encodeURIComponent(data.child)}`,
+      { favorite: data.favorite }
+    );
   }
 
-  public updateFavorite(data: { id: string, favorite: boolean }): Observable<any> {
-    return this.httpClient.post('', data);
-  }
-
-  private groupAndSortByDay(entries: Timeline, filter: (entry: TimelineEntry) => boolean): GroupedTimeline {
+  private groupAndSortByDay(
+    entries: Timeline,
+    filter: (entry: TimelineEntry) => boolean
+  ): GroupedTimeline {
     entries.sort((a, b) => b.timestamp - a.timestamp);
 
     let currentDayTimestamp = this.getStartOfDay(entries[0].timestamp);
-    return entries
-        .filter(filter)
-        .reduce((accumulator, current) => {
+    return entries.filter(filter).reduce((accumulator, current) => {
       currentDayTimestamp = this.getStartOfDay(current.timestamp);
 
       if (accumulator[currentDayTimestamp]) {
@@ -46,6 +63,8 @@ export class TimelineService {
   }
 
   private getStartOfDay(timestamp: number) {
-    return Math.trunc(timestamp / this.millisecondsInADay) * this.millisecondsInADay;
+    return (
+      Math.trunc(timestamp / this.millisecondsInADay) * this.millisecondsInADay
+    );
   }
 }
